fc814ff70231b1e47551cd300684f59364b12c84
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-10-22 03:58:48 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 3, 5, "skils", True, 2.5) # кортеж
+print(immutable_var)
+# immutable_var[1] = 2 # в кортеж используется для хранения данных в неизменном виде
+mutable_list = [2, 4, 6] # список
+mutable_list[1] = 8 # изменение элемента в списке
+print(mutable_list)
